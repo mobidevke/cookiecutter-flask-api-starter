a8bb4387b73ba4c59f589f82e6573f14b3f2d226
Michael Bukachi <michaelbukachi@gmail.com>
2019-05-07 13:15:41 +0300

    Initial commit

---
+++ {{cookiecutter.repo_name}}/README.md
@@ -0,0 +1,7 @@
+## {{cookiecutter.api_name}}
+
+### Project Setup
+
+1. `pip install -r requirements.txt`
+2. `chmod +x unit-tests.sh`
+3. `./unit-tests.sh`
---
+++ {{cookiecutter.repo_name}}/app/config.py
@@ -0,0 +1,50 @@
+import os
+
+POSTGRES = {
+    'user': os.getenv('POSTGRES_USER', 'postgres'),
+    'pw': os.getenv('POSTGRES_PASSWORD', 'root'),
+    'db': os.getenv('POSTGRES_DB', 'postgres'),
+    'host': os.getenv('POSTGRES_HOST', 'localhost'),
+    'port': os.getenv('POSTGRES_PORT', 5432),
+}
+
+TEST_POSTGRES = {
+    'user': os.getenv('POSTGRES_USER', 'postgres'),
+    'pw': os.getenv('POSTGRES_PASSWORD', 'root'),
+    'db': os.getenv('POSTGRES_DB', 'postgres'),
+    'host': os.getenv('POSTGRES_HOST', 'localhost'),
+    'port': os.getenv('POSTGRES_PORT', 5432),
+}
+
+
+class Config:
+    ERROR_404_HELP = False
+
+    SECRET_KEY = os.getenv('APP_SECRET', '79ec24ba5a8dc15a8f5ccb847e555b3a66936314ef531e565fa1973544026cac')
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(**POSTGRES)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    DOC_USERNAME = 'api'
+    DOC_PASSWORD = 'password'
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(**TEST_POSTGRES)
+    TESTING = True
+    DEBUG = True
+
+
+class ProdConfig(Config):
+    DEBUG = False
+
+
+config = {
+    'development': DevConfig,
+    'testing': TestConfig,
+    'production': ProdConfig
+}
